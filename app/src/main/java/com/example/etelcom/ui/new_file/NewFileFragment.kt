f1d607193f5e0aa23f5ce90b6053ed5f4a107b73
marie-nde <marie9nde@gmail.com>
2020-09-14 15:20:40 +0200

    New feature: Date picker

--- app/src/main/java/com/example/etelcom/ui/new_file/NewFileFragment.kt
+++ app/src/main/java/com/example/etelcom/ui/new_file/NewFileFragment.kt
@@ -1,13 +1,15 @@
 package com.example.etelcom.ui.new_file
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatImageButton
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.example.etelcom.R
 import java.text.DateFormat
 import java.text.SimpleDateFormat
@@ -15,20 +17,14 @@ import java.util.*
 
 
 class NewFileFragment : Fragment() {
-    private lateinit var newFileViewModel: NewFileViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        newFileViewModel =
-            ViewModelProviders.of(this).get(NewFileViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_new_file, container, false)
-        val textClient: TextView = root.findViewById(R.id.text_client)
-        newFileViewModel.text.observe(viewLifecycleOwner, Observer {
-            textClient.text = it
-        })
+
         // Gets the date of the device
         val dateFormat: DateFormat = SimpleDateFormat("dd/MM/yyyy")
         val newDate = Date()
@@ -36,6 +32,26 @@ class NewFileFragment : Fragment() {
         val dateToFill: TextView = root.findViewById(R.id.date)
         dateToFill.text = "$currentDate"
 
+        val editDate: TextView = root.findViewById(R.id.date)
+        val calendarBtn: AppCompatImageButton = root.findViewById(R.id.calBtn)
+        calendarBtn.setOnClickListener {
+            // Get Current Date
+            val c = Calendar.getInstance()
+            val mYear = c[Calendar.YEAR]
+            val mMonth = c[Calendar.MONTH]
+            val mDay = c[Calendar.DAY_OF_MONTH]
+            val datePickerDialog = DatePickerDialog(
+                requireActivity(),
+                OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                    val dayWithLeadingZero = String.format("%02d", dayOfMonth)
+                    val monthWithLeadingZero = String.format("%02d", monthOfYear + 1)
+                    editDate.setText(
+                        dayWithLeadingZero + "/" + monthWithLeadingZero + "/" + year
+                    )
+                }, mYear, mMonth, mDay
+            )
+            datePickerDialog.show()
+        }
         return root
     }
 }
