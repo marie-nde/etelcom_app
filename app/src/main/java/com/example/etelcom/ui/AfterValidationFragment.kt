6cdc9b7bcd572b720607646ea1669b0145939173
marie-nde <marie9nde@gmail.com>
2020-09-17 17:05:47 +0200

    Backlash

--- app/src/main/java/com/example/etelcom/ui/AfterValidationFragment.kt
+++ app/src/main/java/com/example/etelcom/ui/AfterValidationFragment.kt
@@ -4,10 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.example.etelcom.R
+
 
 class AfterValidationFragment : Fragment() {
 
@@ -17,7 +16,6 @@ class AfterValidationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_after_validation, container, false)
-
         return root
     }
 }
